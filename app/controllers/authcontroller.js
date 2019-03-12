b8c1393885d341b74002d303ad940cc0d17cef55
eingurat <e.ingurat@bsmart-technology.com>
2019-03-12 20:51:57 +0300

    _crsf prevention implementation

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -1,20 +1,28 @@
 var exports = module.exports = {}
  
 exports.signup = function(req, res) {
-    res.render('signup');
+    res.render('signup', {
+      _csrfToken = req.csrfToken()
+    });
 }
 
 exports.signin = function(req, res) {
-    res.render('signin');
+    res.render('signin', {
+      _csrfToken = req.csrfToken()
+    });
 }
 
 exports.dashboard = function(req,res){
-	res.render('dashboard'); 
+	res.render('dashboard', {
+    _csrfToken = req.csrfToken()
+  }); 
 }
 
 exports.logout = function(req,res){
   req.session.destroy(function(err) {
-  res.redirect('/signin');
+  res.redirect('/signin', {
+    _csrfToken = req.csrfToken()
+  });
   });
 
 }
