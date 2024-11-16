9dafea7132e6da3b7a907675fffb9ebe7eddbc08
Emmy Ingurat <eingurat504@gmail.com>
2024-11-16 14:21:34 +0300

    removed unneccessary comments

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,10 +3,6 @@ exports.index = async(req,res) => {
 
     const db = require("../models");
     const User = db.users;
-    // const { User } = require('../models/user.js');
-    // const { User } = require('../models');
-
-      // try {
 
       User.findAll()
       .then(data => {
@@ -19,20 +15,5 @@ exports.index = async(req,res) => {
         });
       });
 
-        // const users = await User.findAll;
-// 
-        // console.log(users);
-
-        // Convert the results to plain objects (needed for Sequelize v4)
-        // const usersData = users.map(user => user.get({ plain: true }));
-    
-        // Log the data for debugging
-        // console.log('Users:', usersData);
-
-        // res.render('./users/index', { title: 'User List', users });
-      // } catch (err) {
-      //   console.error(err);
-      //   res.status(500).send('Error retrieving users');
-      // }
 }
 
